545036e3b94d60b043a828565daebdd63a0aaccb
IngAcoralesP <Acorales-hub@users.noreply.github.com>
2025-02-21 14:28:52 -0400

    Create function openJsInfo

--- public/javascrip-for-website.js
+++ public/javascrip-for-website.js
@@ -11,3 +11,10 @@
             sl[i].rel = 'noopener noreferrer';
         }
     }
+
+    function openJsInfo(){
+        const params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
+        width=1200,height=500,left=90,top=100`;//For popUp
+        const csUrl = 'https://es.javascript.info/'
+        open(csUrl, "test", params);
+    }
