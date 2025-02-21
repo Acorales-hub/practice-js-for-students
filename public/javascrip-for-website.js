049b64fc513bbfe9642c2da832da8bb9602efd3b
IngAcoralesP <Acorales-hub@users.noreply.github.com>
2025-02-21 16:14:06 -0400

    Create function codeEditor

--- public/javascrip-for-website.js
+++ public/javascrip-for-website.js
@@ -18,3 +18,10 @@
         const csUrl = 'https://es.javascript.info/'
         open(csUrl, "test", params);
     }
+
+    function codeEditor(){
+      const params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
+      width=1250,height=510,left=70,top=90`;//For popUp
+      const csUrl = 'https://www.mycompiler.io/es/new/nodejs'
+      open(csUrl, "test", params);
+    }
