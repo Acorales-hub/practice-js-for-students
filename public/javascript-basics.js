b83ca74abcc7506e1145533028c2f530783dd658
IngAcoralesP <Acorales-hub@users.noreply.github.com>
2025-02-21 17:53:09 -0400

    Add link Variables -> index.html and add typeOfvars in javascript-basics.js

--- public/javascript-basics.js
+++ public/javascript-basics.js
@@ -1,22 +1,28 @@
 'use strict'
-//Global vars
+//Global variables
     let showResp = document.getElementById('show')
-    const params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
-    width=1200,height=500,left=90,top=100`;//For popUp
+    const params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=1200,height=500,left=90,top=100`;//For popUp
 
-    function greet(){//greet (saludar)
-        alert('Hello Coders!')
-        showResp.innerHTML = '<h1 class="display-5 fw-bold">Hello, coders world!</h1><p class="col-md-8 fs-4">Hello Coders from the function greet.</p>'
-    }
+    
+    //Funtions:
+        function greet(){//greet (saludar)
+            alert('Hello Coders!')
+            showResp.innerHTML = '<h1 class="display-5 fw-bold">Hello, coders world!</h1><p class="col-md-8 fs-4">Hello Coders from the function greet.</p>'
+        }
 
-    function codeStructure(){
-        const csUrl = 'https://es.javascript.info/structure'
+        function codeStructure(){
+            const csUrl = 'https://es.javascript.info/structure'
+            open(csUrl, "test", params);
+        }
+
+        function useStrict(){
+        const csUrl = 'https://es.javascript.info/strict-mode'
         open(csUrl, "test", params);
-    }
+        }
 
-    function useStrict(){
-      const csUrl = 'https://es.javascript.info/strict-mode'
-      open(csUrl, "test", params);
-    }
+        function typeOfVars(){
+            const csUrl = 'https://es.javascript.info/variables'
+            open(csUrl,'test',params)
+        }
 
     
